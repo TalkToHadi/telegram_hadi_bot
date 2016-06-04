899d26f1af002b5d3e6dd637bec5330079fe52fb
Angel Villalba <angelvil92@gmail.com>
2016-06-05 01:20:27 +0200

    Changes the states

--- hadi.py
+++ hadi.py
@@ -4,201 +4,14 @@ from telebot import types
 TOKEN = '239519305:AAH-1nJiWwqyigWQvg-qxZKpo6nnKpGh2sU' #Ponemos nuestro TOKEN generado con el @BotFather
 bot = telebot.TeleBot(TOKEN)
 
-
-"""
-class Critical(State):
-    message = (
-        "Please seek help inmediatly"
-    )
-
-class NoHelp(State):
-    message = (
-        "Sorry, I can't help you with this"
-    )
-
-class Burocrazy(State):
-    message = (
-        "Sorry, I can't help you with this but I know who can!"
-        "You should check Burocrazy, those guys are cool"
-    )
-
-class Oops(State):
-    message = (
-        "Oops, I'm sorry to hear that"
-        "Talk to me later if you want"
-    )
-
-class Introduce(State):
-    message = (
-        "Let me introduce you to my friend Hans!"
-    )
-
-class FeelUp(State):
-    
-
-
-class HotLine(State):
-    message = (
-        "You need inmediate help!"
-        "Call the emergency hotline"
-        "http://www.suicide.org/hotlines/international/germany-suicide-hotlines.html"
-        "http://www.frauenhauskoordinierung.de/english-summary.html"
-    )
-
-
-class FunnyVideos(State):
-    message = (
-        "Check this web page!"
-        "https://www.youtube.com/results?search_query=funny"
-    )
-
-class HeadSpace(State):
-    message = (
-        "Try an app like HeadSpace to learn how to meditate"
-    )
-
-
-class Cinema(State):
-    message = (
-        "Here you can find cinemas around you!"
-        "https://www.google.com/maps?q=cinema"
-    )
-
-
-class Sports(State):
-    message = (
-        "Here you can find sports events (and more)!"
-        "https://www.sportsworld.co.uk/events"
-    )
-
-
-class Bored3(State):
-    options = {
-        "Yeah, I'd love to!": Cinema,
-        "Not right now": Sports,
-    }
-    message = (
-        "Do you prefer to go to cinema?"
-    )
-
-
-class Bored2(State):
-    options = {
-        "Yes, of course!": FunnyVideos,
-        "Not really": Bored3,
-    }
-    message = (
-        "What about watching some funny videos?"
-    )
-
-class MeetUp(State):
-    message = (
-    "Check MeetUp! You'll find something you like for sure"
-    "http://www.meetup.com/"
-    )
-
-
-class Bored(State):
-    options = {
-        "Yeah, sure!": MeetUp,
-        "Not right now": Bored2,
-    }
-
-    message = (
-        "Would you like to meet people with your same interests?"
-    )
-
-
-class Tired(State):
-    options = {
-        "I'm mentally exhausted": HeadSpace,
-        # "I feel my body has no energy": Sleeping,
-    }
-    message = (
-        "Why are you tired?"
-    )
-
-
-class Ill(State):
-    message = (
-        "Look this awesome link to german national health system"
-    )
-
-
-
-class TedTalk(State):
-    message = (
-        "Watch this inpirational talks on how to deal with feeling lonely"
-        "http://www.keepinspiring.me/25-creative-and-surprising-things-to-do-when-you-feel-lonely/"
-    )
-
-
-class GoogleMaps(State):
-    message = (
-        "Try searching at GoogleMaps :)"
-        "https://www.google.com/maps"
-    )
-
-class HomeSick(State):
-    options = {
-        "Some food would be great": GoogleMaps,
-        "I would like to find cool things to do around": MeetUp,
-    }
-    message = (
-        "It's hard to be away from home"
-    )
-
-class Lonely(State):
-    options = {
-        "Yeah, I would like talk with someone": Introduce,
-        "It would be cool to join some activity": MeetUp,
-        "Not right now": TedTalk,
-    }
-
-    message = (
-        "Would you like to meet new people?"
-    )
-
-class Lost(State):
-    options = {
-        "I want to go somewhere": GoogleMaps,
-        "I need help with my paperwork": Burocrazy,
-        "Someone to talk to": Lonely,
-    }
-
-    message = (
-        "What are you looking for?"
-    )
-
-
-class FeelDown(State):
-    options = {
-        "I'm bored": Bored,
-        # "I'm sad": Sad,
-        "I'm stressed with paperwork": Burocrazy,
-        "I'm lost": Lost,
-        "I'm tired": Tired,
-        "I miss my country": HomeSick,
-        "I feel lonely": Lonely,
-        # "I'm angry": Angry,
-        "I'm feeling ill": Ill,
-        "None of that": NoHelp,
-    }
-
-    message = (
-        "So sorry to hear that"
-        "Whats wrong?"
-    )
-"""
-
 STATES = {
     'Initial': {
         'message': (
             "Hi there. My name is Hadi, I'm here for you. What's up?"
-        )
+        ),
         'options': [
             "I'm feeling up! :D",
-            "Not feelin great today :(",
+            "Not feeling great today :(",
         ]
     },
     "I'm feeling up! :D": {
@@ -211,7 +24,254 @@ STATES = {
             "Maybe on another time",
         ]
     },
+    "Not feelin great today :(": {
+        'message': ('Ooops'),
+        'options': []
+    },
+    "I'm stressed with paperwork": {
+        'message': (
+            "Sorry, I can't help you with this but I know who can!"
+            "You should check Burocrazy, those guys are cool"
+        ),
+        'options': [
+
+        ]
+    },
+    "I need help with my paperwork": {
+        'message': (
+            "Sorry, I can't help you with this but I know who can!"
+            "You should check Burocrazy, those guys are cool"
+        ),
+        'options': [
+
+        ]
+    },
+    "None of that": {
+        'message': (
+            "Sorry, I can't help you with this"
+        ),
+        'options': [
+
+        ]
+    },
+    "Maybe on another time": {
+        'message': (
+            "Oops, I'm sorry to hear that"
+            "Talk to me later if you want"
+        ),
+        'options': [
+
+        ]
+    },
+    "Yeah, I would like talk with someone": {
+        'message': (
+            "Let me introduce you to my friend Hans!"
+        ),
+        'options': [
+
+        ]
+    },
+    "HotLine": {
+        'message': (
+            "You need inmediate help!"
+            "Call the emergency hotline"
+            "http://www.suicide.org/hotlines/international/germany-suicide-hotlines.html"
+            "http://www.frauenhauskoordinierung.de/english-summary.html"
+        ),
+        'options': [
+
+        ]
+    },
+    "Yes, of course!": {
+        'message': (
+            "Check this web page!"
+            "https://www.youtube.com/results?search_query=funny"
+        ),
+        'options': [
+
+        ]
+    },
+    "I'm mentally exhausted": {
+        'message': (
+            "Try an app like HeadSpace to learn how to meditate"
+        ),
+        'options': [
+
+        ]
+    },
+    "Yeah, I'd love to!": {
+        'message': (
+            "Here you can find cinemas around you!"
+            "https://www.google.com/maps?q=cinema"
+        ),
+        'options': [
+
+        ]
+    },
+    "Not right now": {
+        'message': (
+            "Here you can find sports events (and more)!"
+            "https://www.sportsworld.co.uk/events"
+        ),
+        'options': [
+
+        ]
+    },
+    "Not really": {
+        'message': (
+            "Do you prefer to go to cinema?"
+        ),
+        'options': [
+            "Yeah, I'd love to!",
+            "Not right now",
+        ]
+    },
+    "Not right now": {
+        'message': (
+            "What about watching some funny videos?"
+        ),
+        'options': [
+            "Yes, of course!",
+            "Not really",
+        ]
+    },
+    "Yeah, sure!": {
+        'message': (
+            "Check MeetUp! You'll find something you like for sure"
+            "http://www.meetup.com/"
+        ),
+        'options': [
+
+        ]
+    },
+    "I would like to find cool things to do around": {
+        'message': (
+            "Check MeetUp! You'll find something you like for sure"
+            "http://www.meetup.com/"
+        ),
+        'options': [
+
+        ]
+    },
+    "It would be cool to join some activity": {
+        'message': (
+            "Check MeetUp! You'll find something you like for sure"
+            "http://www.meetup.com/"
+        ),
+        'options': [
+
+        ]
+    },
+    "I'm bored": {
+        'message': (
+            "Would you like to meet people with your same interests?"
+        ),
+        'options': [
+            "Yeah, sure!",
+            "Not right now",
+        ]
+    },
+    "I'm tired": {
+        'message': (
+            "Why are you tired?"
+        ),
+        'options': [
+            "I'm mentally exhausted",
+        ]
+    },
+    "I'm feeling ill": {
+        'message': (
+            "Look this awesome link to german national health system"
+        ),
+        'options': [
+
+        ]
+    },
+    "Not right now": {
+        'message': (
+            "Watch this inpirational talks on how to deal with feeling lonely"
+            "http://www.keepinspiring.me/25-creative-and-surprising-things-to-do-when-you-feel-lonely/"
+        ),
+        'options': [
+
+        ]
+    },
+    "Some food would be great": {
+        'message': (
+            "Try searching at GoogleMaps :)"
+            "https://www.google.com/maps"
+        ),
+        'options': [
+
+        ]
+    },
+    "I want to go somewhere": {
+        'message': (
+            "Try searching at GoogleMaps :)"
+            "https://www.google.com/maps"
+        ),
+        'options': [
+
+        ]
+    },
+    "I miss my country": {
+        'message': (
+            "It's hard to be away from home"
+        ),
+        'options': [
+            "Some food would be great",
+            "I would like to find cool things to do around",
+        ]
+    },
+    "Someone to talk to": {
+        'message': (
+            "Would you like to meet new people?"
+        ),
+        'options': [
+            "Yeah, I would like talk with someone",
+            "It would be cool to join some activity",
+            "Not right now",
+        ]
+    },
+    "I feel lonely": {
+        'message': (
+            "Would you like to meet new people?"
+        ),
+        'options': [
+            "Yeah, I would like talk with someone",
+            "It would be cool to join some activity",
+            "Not right now",
+        ]
+    },
+    "I'm lost": {
+        'message': (
+            "I want to go somewhere",
+            "I need help with my paperwork",
+            "Someone to talk to",
+        ),
+        'options': [
+            "What are you looking for?"
+        ]
+    },
+    "Not feeling great today :(": {
+        'message': (
+            "So sorry to hear that"
+            "Whats wrong?"
+        ),
+        'options': [
+            "I'm bored",
+            "I'm stressed with paperwork",
+            "I'm lost",
+            "I'm tired",
+            "I miss my country",
+            "I feel lonely",
+            "I'm feeling ill",
+            "None of that",
+        ]
+    },
 }
+
+
 
 def markup(state):
     markup = types.ReplyKeyboardMarkup()
@@ -228,10 +288,10 @@ def send_welcome(answer):
         initial['message'],
         reply_markup=markup(initial))
 
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(func=lambda m: m.text!=STATES['Initial']['message'])
 def handle_message(answer):
     chat_id = answer.chat.id
-    next_state = STATES[answer.text]
+    next_state = STATES.get(answer.text, STATES['Initial'])
     bot.send_message(
         chat_id,
         next_state['message'],
